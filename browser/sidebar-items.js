6e15f7de8adcedd79a04a45b0ae08279ea8d2cc4
realaravinth <realaravinth@batsense.net>
2021-02-28 18:08:31 +0000

    Deploying to gh-pages from  @ c58117300381c67f5c5aed8450c0e61fe8726e98 🚀

--- browser/sidebar-items.js
+++ browser/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["__wasm_bindgen_generated_greet",""],["greet",""]]});
+initSidebarItems({"fn":[["__wasm_bindgen_generated_gen_pow",""],["gen_pow",""]]});
--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["browser"] = {"name":"","files":["lib.rs","utils.rs"]};
+sourcesIndex["browser"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["cli"] = {"name":"","files":["main.rs"]};
 sourcesIndex["m_captcha"] = {"name":"","files":["counter.rs","errors.rs","levels.rs","lib.rs"]};
 createSourceSidebar();
