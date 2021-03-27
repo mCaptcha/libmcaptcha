ea1ec97c7887c6872d31f7aa6dbf90fb8d7b4752
realaravinth <realaravinth@batsense.net>
2021-03-27 09:04:09 +0000

    Deploying to gh-pages from  @ ab2f418b571f41adb2c42414fed4415761a3fa52 🚀

--- implementors/core/convert/trait.From.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["m_captcha"] = [{"text":"impl From&lt;Work&gt; for PoW&lt;String&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["m_captcha"] = [{"text":"impl Display for CaptchaError","synthetic":false,"types":[]}];
+implementors["m_captcha"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"m_captcha/errors/enum.CaptchaError.html\" title=\"enum m_captcha::errors::CaptchaError\">CaptchaError</a>","synthetic":false,"types":["m_captcha::errors::CaptchaError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
