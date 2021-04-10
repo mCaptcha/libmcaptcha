9b64d6f7313cd0c6af58def64894971a2e03c5f9
realaravinth <realaravinth@batsense.net>
2021-04-10 06:14:45 +0000

    Deploying to gh-pages from  @ 6f83b3cd0c35a1244db21c0061f9211918b9cdef 🚀

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["m_captcha"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"m_captcha/cache/messages/struct.CachedPoWConfig.html\" title=\"struct m_captcha::cache::messages::CachedPoWConfig\">CachedPoWConfig</a>&gt; for <a class=\"struct\" href=\"m_captcha/cache/messages/struct.CacheResult.html\" title=\"struct m_captcha::cache::messages::CacheResult\">CacheResult</a>","synthetic":false,"types":["m_captcha::cache::messages::CacheResult"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
