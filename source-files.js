6a5d88002b6aa71c05a0bd9315a6c5c2b0b22b46
realaravinth <realaravinth@batsense.net>
2021-06-03 12:41:14 +0000

    Deploying to gh-pages from  @ b0cf17aaa56bb701268fddfa164005a341ba1a7f 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["libmcaptcha"] = {"name":"","dirs":[{"name":"cache","files":["hashcache.rs","mod.rs"]}],"files":["defense.rs","errors.rs","lib.rs","master.rs","mcaptcha.rs","pow.rs","system.rs","utils.rs"]};
+sourcesIndex["libmcaptcha"] = {"name":"","dirs":[{"name":"cache","files":["hashcache.rs","mod.rs"]},{"name":"master","files":["embedded.rs","mod.rs"]}],"files":["defense.rs","errors.rs","lib.rs","mcaptcha.rs","pow.rs","system.rs","utils.rs"]};
 createSourceSidebar();
