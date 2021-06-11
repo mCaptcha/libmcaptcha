75d851edfc7d265ad830b8c4760453159564255a
realaravinth <realaravinth@batsense.net>
2021-06-11 06:40:57 +0000

    Deploying to gh-pages from  @ 2ff85dfdbcc0e595a4342f30daae7a822ee68554 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["libmcaptcha"] = {"name":"","dirs":[{"name":"cache","files":["hashcache.rs","mod.rs"]},{"name":"master","dirs":[{"name":"embedded","files":["counter.rs","master.rs","mod.rs"]},{"name":"redis","files":["master.rs","mod.rs"]}],"files":["mod.rs"]},{"name":"redis","files":["mcaptcha_redis.rs","mod.rs"]}],"files":["defense.rs","errors.rs","lib.rs","mcaptcha.rs","pow.rs","system.rs","utils.rs"]};
+sourcesIndex["libmcaptcha"] = {"name":"","dirs":[{"name":"cache","files":["hashcache.rs","mod.rs","redis.rs"]},{"name":"master","dirs":[{"name":"embedded","files":["counter.rs","master.rs","mod.rs"]},{"name":"redis","files":["master.rs","mod.rs"]}],"files":["mod.rs"]},{"name":"redis","files":["mcaptcha_redis.rs","mod.rs"]}],"files":["defense.rs","errors.rs","lib.rs","mcaptcha.rs","pow.rs","system.rs","utils.rs"]};
 createSourceSidebar();
