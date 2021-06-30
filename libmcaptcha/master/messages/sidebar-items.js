50422450aa8cbadd9f64dc4b3a65689faceee1ba
realaravinth <realaravinth@batsense.net>
2021-06-30 14:40:08 +0000

    Deploying to gh-pages from  @ 60107a2b6f5b1b80c7351a42e05c8c0c6cdbf5de 🚀

--- libmcaptcha/master/messages/sidebar-items.js
+++ libmcaptcha/master/messages/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AddSite","Message to add an [Counter] actor to [Master]"],["AddSiteBuilder","Builder for `AddSite`."],["AddVisitor","Message to add visitor to an [MCaptcha] actor"]]});
+initSidebarItems({"enum":[["AddSiteBuilderError","Error type for AddSiteBuilder"]],"struct":[["AddSite","Message to add an [Counter] actor to [Master]"],["AddSiteBuilder","Builder for `AddSite`."],["AddVisitor","Message to add visitor to an [MCaptcha] actor"]]});
