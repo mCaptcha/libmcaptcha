ab0f5a21bc76551944179f751002d4985a47aa75
realaravinth <realaravinth@batsense.net>
2023-10-16 17:27:53 +0000

    Deploying to gh-pages from  @ 5acab0b02746c4fd07932ac810186adf9a623aff 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "libmcaptcha":["",[["cache",[],["hashcache.rs","mod.rs","redis.rs"]],["master",[["embedded",[],["counter.rs","master.rs","mod.rs"]],["redis",[],["master.rs","mod.rs"]]],["mod.rs"]],["redis",[],["mcaptcha_redis.rs","mod.rs"]]],["defense.rs","errors.rs","lib.rs","mcaptcha.rs","pow.rs","queue.rs","system.rs","utils.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
